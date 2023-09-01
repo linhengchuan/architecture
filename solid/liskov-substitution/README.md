5cbfabe393f71c69ec186d58c9b821f31862cbef
Heng Chuan Lin <linhengchuan@gmail.com>
2023-09-01 19:29:18 +0800

    Add lsp code

---
+++ solid/liskov-substitution/README.md
@@ -0,0 +1,9 @@
+# (L)iskov-substitution principle(LSP)
+
+- Objects should be replaceable with their subtype without affecting the correctness of the program.
+- For example a class penguine inherits class bird. Penguine (is-a) bird.
+- Bird object has fly method while penguine has no fly method(overwrite and unimplemented).
+- If we replace bird object with penguine object, the code fails when fly is called.
+- LSP is failed in this case.
+
+# LSP is a more strict test than IS-A test
---
+++ solid/liskov-substitution/penguine_bird.py
@@ -0,0 +1,30 @@
+# Penguine (is-a) bird. Thus we allow penguine to inherit bird class.
+# However there is a problem with this relationship. Penguine is not able
+# to fly like most birds do. We need to over write the fly function and
+# unimplement it. This is a sign of design flaws.
+# Penguine (is-a) bird is correct statement but it is against the liskov
+# substitution principle which test whether a class can be substituted by
+# its subtype(In this case bird class will ).
+
+
+class bird():
+    def __init__(self) -> None:
+        pass
+
+    def fly(self):
+        print("Fly!")
+
+
+class penguine(bird):
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def fly(self):
+        # Unimplemented because penguine cant fly
+        print("Penguine cant fly.")
+        pass
+
+
+a = penguine()
+a.fly()
