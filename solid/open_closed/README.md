921b83798a764e913d3eb0959a6ce46cd98e264f
Heng Chuan Lin <linhengchuan@gmail.com>
2023-09-01 18:01:37 +0800

    Update open close principle code

--- solid/open_closed/README.md
+++ solid/open_closed/README.md
@@ -1,8 +1,14 @@
-
 # (O)pen Close principle. Close for modification but open for extension.
 
 ### Closed for modification
+
 - New features getting added to the software component should not have to modify existing code.
 
 ### Open for extension
+
 - A software component should be extendable to add a new feature or add a new behaviour.
+
+### Note
+
+- Do not over use this principle and create too many classes.
+- Consider whether this principle will help to reduce bug fixing before implementing.
--- solid/open_closed/open_closed.py
+++ solid/open_closed/open_closed.py
@@ -1,35 +1,82 @@
 """
-Initially ProjectWork class only takes in 
+Initially ProjectWork class only takes in Employee class.
+However there might be future addition of PartTimer class.
+Thus the architecture should be design such that ProjectWork class is
+close for modification but it can be extended for more features.
 """
+
 
 class ProjectWork():
     from typing import overload
-    @overload
-    def calculate_success_rate_a(self, member: 'Employee'): # only for illustration purpose, python type check does not restrict data type.
+    # only for illustration purpose, python type check does not restrict
+    # datatype.
+
+    def calculate_success_rate_a(self, member: 'Employee'):
         print("Employee function")
         return member.get_skill_level()
-    
-    @overload
+
     def calculate_success_rate_b(self, member: 'PartTimer'):
         print("PartTimer function")
         return member.get_skill_level()
 
+
 class Employee():
     def __init__(self) -> None:
         self._skill_level = 5
-    
+
     def get_skill_level(self):
-        return self._skill_level 
+        return self._skill_level
+
 
 class PartTimer():
     def __init__(self) -> None:
         self._skill_level = 1
-    
+
     def get_skill_level(self):
-        return self._skill_level 
-    
+        return self._skill_level
+
+
 a = ProjectWork()
 b = Employee()
 c = PartTimer()
 a.calculate_success_rate_a(b)
 a.calculate_success_rate_b(c)
+
+"""
+In the new class, we create a base class that can be inherited so that more
+types of workers can be added without changing the original code.
+Note: In other language, create a interface that can be implemented
+by other classes.
+"""
+
+
+class NewProjectWork():
+    from typing import overload
+
+    # only for illustration purpose, python type check does not restrict data
+    # type.
+    def calculate_success_rate(self, member: 'People'):
+        return member.get_skill_level()
+
+
+class People():
+    def __init__(self) -> None:
+        self._name = ""
+
+
+class NewEmployee(People):
+    def __init__(self) -> None:
+        super.__init__()
+        self._skill_level
+
+    def get_skill_level(self):
+        return self._skill_level
+
+
+class NewPartTimer():
+    def __init__(self) -> None:
+        super.__init__()
+        self._skill_level = 1
+
+    def get_skill_level(self):
+        return self._skill_level
